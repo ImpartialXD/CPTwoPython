bc22a44b6b1ff164587fdf6e7b5a742d79a41d50
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-02-23 13:03:53 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+# item = "Potato chips"
+# quantity = 10
+# price = 5.99
+# isFood = True
+# print(type(item))
+# print(type(quantity))
+# print(type(price))
+# print(type(isFood))
+# #Type Casting
+
+# numberStr = "10.15"
+# print(numberStr)
+# print(type(numberStr))
+# numberStr = float(numberStr)
+# print(numberStr)
+# print(type(numberStr))
+# Firstname = input("Enter first name: ")
+# age = int(input("Enter your age: "))
+# if age <=12:
+# print("bata")
+# else:
+# print("hindi bata")
+
+#Exercise One
+
+# width = float(input("Enter value for width: "))
+# length = float(input("Enter value for length: "))
+
+# area = width * length
+
+# print(f"Area is = {area}")
+
+#Exercise Two
+
+# item = input("What item would you like to buy? ")
+# price = float(input("Whats the price? "))
+# quantity = float(input("How many would like to buy? "))
+
+# totalPrice = price * quantity
+
+# print(f"\n Item: {item}")
+# print(f"\n Price: {price}")
+# print(f"\n Quantity: {quantity}")
+
+# print(f"\n The Total Price is: {totalPrice}")
